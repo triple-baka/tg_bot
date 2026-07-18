92991750a10cb5eff0ae06e65f1e7d641d6234ca
Admin <prymmat@gmail.com>
2026-07-18 19:22:45 +0100

    removed print, somehow fixed the recurring deletion

--- stripe_webhook.py
+++ stripe_webhook.py
@@ -151,7 +151,6 @@ async def stripe_webhook(request: Request):
     elif event_type == "customer.subscription.deleted":
 
         subscription = event["data"]["object"]
-        print(subscription)
 
         telegram_user_id = int(subscription.metadata["telegram_user_id"])
 
